bc32f6d56b5597cb8e3f23a94e34de653d86b253
DennisJav <denis.javier.tapia.ortiz@gmail.com>
2022-06-14 19:58:53 -0500

    Taller 11, SCOPE PROTOTYPE SINGLETON

--- src/main/java/com/example/ProyectoU1DtApplication.java
+++ src/main/java/com/example/ProyectoU1DtApplication.java
@@ -1,7 +1,5 @@
 package com.example;
 
-
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -24,32 +22,27 @@ import com.example.mega.service.IGestorInventario;
 import com.example.modelo.Estudiante;
 import com.example.modelo.Materia;
 import com.example.modelo.Matricula;
+import com.example.modelo.ProfesorGeneral;
+import com.example.modelo.ProfesorMateria;
 import com.example.service.IEstudianteService;
 import com.example.service.IMateriaService;
 import com.example.service.IMatriculaService;
 
 @SpringBootApplication
 public class ProyectoU1DtApplication implements CommandLineRunner {
-	
-//	@Autowired
-//	private ITransferenciaService transferenciaService;
-//
-//	@Autowired
-//	private IDepositoService depositoService;
-//
-//	@Autowired
-//	private ICuentaBancariaService cuentaBancariaService;
-//	
-//	@Autowired
-//	private IRetiroService retiroService;
-	
-//	@Autowired
-//	private IGestorInventario gestorInventario;
-//	
 
 	@Autowired
-	private IFachadaCuentaBancaria fachadaCuentaBancaria;
+	private ProfesorGeneral profesorGeneral;
+	@Autowired
+	private ProfesorGeneral profesorGeneral1;
+
+	@Autowired
+	private ProfesorMateria materia;
+	@Autowired
+	private ProfesorMateria materia1;
 	
+	@Autowired
+	private IMatriculaService matriculaService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1DtApplication.class, args);
@@ -57,10 +50,31 @@ public class ProyectoU1DtApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-	
 
-		BigDecimal interes= this.fachadaCuentaBancaria.calcularInteres("121");
-		System.out.println(interes);
+		// 1
+		System.out.println("SINGLETON");
+		this.profesorGeneral.setNombre("DEnnis");
+		this.profesorGeneral.setApellido("Tapia");
+		System.out.println(this.profesorGeneral);
+		//
+		this.profesorGeneral1.setNombre("JAVIER");
+		System.out.println(this.profesorGeneral);
+
+		System.out.println("PROTOTYPE");
+		this.materia.setNombre("Daniel");
+		this.materia.setApellido("Cano");
+		System.out.println(this.materia);
+		System.out.println("------------");
+		System.out.println(this.materia1);
+		this.materia1.setNombre("Albert");
+		System.out.println(this.materia1);
+//		
+		Matricula m=new Matricula();
+		m.setEstudiante(new Estudiante());
+		m.setMateria(new ArrayList<Materia>());
+		m.setNumero("12");
+		
+		this.matriculaService.insertarMatricula(m);
 		
 	}
 
--- src/main/java/com/example/service/MatriculaServiceImpl.java
+++ src/main/java/com/example/service/MatriculaServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.modelo.Matricula;
+import com.example.modelo.ProfesorGeneral;
+import com.example.modelo.ProfesorMateria;
 import com.example.repository.IMatriculaRepo;
 
 @Service
@@ -11,10 +13,17 @@ public class MatriculaServiceImpl implements IMatriculaService {
 
 	@Autowired
 	private IMatriculaRepo matriculaRepo;
-
+	@Autowired
+	private ProfesorGeneral profesorGeneral;
+	@Autowired
+	private ProfesorMateria profesorMateria;
+	
 	@Override
 	public void insertarMatricula(Matricula matricula) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("DI desde Service SINGLETON"+this.profesorGeneral);
+		System.out.println("DI desde Service PROTOTYPE"+this.profesorMateria);
 		this.matriculaRepo.insertarMatricula(matricula);
 	}
 
